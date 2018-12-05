f0c0e25d62097e13a4af0e5b88f0eb6a81c2715d
Yoann KARTOUT <Yuri@MacBook-Pro.local>
2018-12-05 01:21:03 +0100

    j'en ai marre ca compile pas

--- training.py
+++ training.py
@@ -1,39 +1,34 @@
 #!/usr/bin/env python2.7
 
 import csv
+from utils import sigmatheta0, sigmatheta1, estimate_price
 
-theta0 = 0
-theta1 = 0
-tmptheta0 = 0
-tmptheta1 = 0
-sigma0 = 0
-sigma1 = 0
-m = -1
-list = []
+theta0 = 0.0
+theta1 = 0.0
+tmptheta0 = 0.0
+tmptheta1 = 0.0
+sigmatheta0 = 0.0
+sigmatheta1 = 0.0
+learningrate = 0.0000001
 
-lr = -0.1
+price = []
+mileage = []
 
 with open('data.csv') as f:
-    readCSV = csv.reader(f)
-    for row in readCSV:
-        list.append(row)
-        m += 1
-j = 0
-while j < 10:
-    i = 1
-    # tmptheta0 = theta0
-    # tmptheta1 = theta1
-    while i <= m:
-        sigma0 = sigma0 + ((tmptheta0 + (tmptheta1 * int(list[i][0]))) - int(list[i][1]))
-        sigma1 = sigma0 * int(list[i][0])
-        i += 1
-    # print (sigma0, sigma1)
+	read = csv.reader(f)
+	for r in read:
+		if r[0].isdigit():
+			mileage.append(r[0])
+		if r[1].isdigit():
+			price.append(r[1])
 
-    tmptheta0 = lr * (sigma0 / m)
-    tmptheta1 = lr * (sigma1 / m)
-    print (tmptheta0, tmptheta1)
+mileage = map(float, mileage)
+price = map(float, price)
 
-    theta0 = theta0 - tmptheta0
-    theta1 = theta1 - tmptheta1
-    # print (theta0, theta1)
-    j += 1
+m = float(len(mileage))
+
+for i in range(0, 20):
+	tmptheta0 = learningrate * (sigmatheta0(theta0, theta1, mileage, price, m) / m)
+	tmptheta1 = learningrate * (sigmatheta1(theta0, theta1, mileage, price, m) / m)
+
+print (tmptheta0, tmptheta1)
---
+++ utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2.7
+
+def estimate_price(theta0, theta1, mileage):
+	return (theta0 + (theta1 * mileage))
+
+def	sigmatheta0(theta0, theta1, mileage, price, m):
+	res = 0.0
+	print (type(res))
+	for i in range(0, m):
+		res += estimate_price(theta0, theta1, mileage[i]) - price[i]
+	return (res)
+
+def sigmatheta1(theta0, theta1, mileage, price, m):
+	res = 0.0
+	for i in range(0, m):
+		res += (estimate_price(theta0, theta1, mileage[i]) - price[i]) * mileage[i]
+	return (res)
